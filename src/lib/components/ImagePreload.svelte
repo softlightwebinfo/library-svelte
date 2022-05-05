844561c568f80b469efdce907e41e7f9c80a4f5f
rafa gonzalez <rafael.gonzalez.1737@gmail.com>
2022-05-05 09:39:09 +0200

    add

--- src/lib/components/ImagePreload.svelte
+++ src/lib/components/ImagePreload.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	export let image: string;
-	export let notFound = '/images/not-found.png';
+	const notFound = '/images/not-found.png';
 
 	const preload = async (src) => {
 		const resp = await fetch(src);
 		const blob = await resp.blob();
 
-		return new Promise(function (resolve) {
+		return new Promise(function (resolve, reject) {
 			let reader = new FileReader();
 			reader.readAsDataURL(blob);
-			reader.onload = () => resolve(reader.result);
-			reader.onerror = (error) => reject('Error: ', error);
+			reader.onload = () => {
+				if (reader.result == 'data:') {
+					reject('No se encuentra la imagen');
+					return;
+				}
+				resolve(reader.result);
+			};
+			reader.onerror = (error) => reject('Error 2: ' + error);
 		});
 	};
 </script>
@@ -19,6 +25,6 @@
 	Image is loading!
 {:then base64}
 	<img src={base64} />
-{:catch}
+{:catch e}
 	<img src={notFound} />
 {/await}
