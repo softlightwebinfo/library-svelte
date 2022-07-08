dbaa1504f2c04a2b26188a381354852ddbf77634
Codeunic <rafael.gonzalez.1737@gmail.com>
2022-07-08 17:48:35 +0200

    add

---
+++ src/lib/components/WidgetLoader.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Button from './Button.svelte';
+
+	import TitleHeading from './TitleHeading.svelte';
+	import Toolbar from './Toolbar.svelte';
+	export let title: string;
+</script>
+
+<div class="WidgetLoader">
+	<header>
+		<Toolbar spaceBetween center>
+			<TitleHeading>{title}</TitleHeading>
+			<Button size="lg" icon="fa fa-arrow-right-rotate" on:click />
+		</Toolbar>
+	</header>
+	<section>
+		<slot />
+	</section>
+</div>
+
+<style lang="scss">
+	.WidgetLoader {
+		--bs-primary-rgb: var(--bs-secondary);
+	}
+    section{
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+</style>
---
+++ src/routes/widget-loader.svelte
@@ -0,0 +1,22 @@
+<script>
+	import WidgetLoader from './../lib/components/WidgetLoader.svelte';
+	import Accordion from '$lib/components/Accordion.svelte';
+	import Badge from '$lib/components/Badge.svelte';
+
+	const data = {
+		foo: [1, 2, 3],
+		bar: [4, 5, 6, 7, 8, 9, 10, 11],
+		baz: [12, 24, 42]
+	};
+</script>
+
+<WidgetLoader title="Trending" on:click={console.log}>
+	<Badge secondary round># Hello</Badge>
+	<Badge secondary round># Hello</Badge>
+	<Badge secondary round># Hello</Badge>
+	<Badge secondary round># Hello</Badge>
+</WidgetLoader>
+
+<style lang="scss" global>
+	@import '../lib/style.scss';
+</style>
